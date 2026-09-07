83a5e41cbc748e0388990c81a12fd6a616a6bf74
cloudjohnsp <jvavelino2017@gmail.com>
2026-09-06 23:11:38 -0300

    test: implements getbyid query tests for user entity

---
+++ ECommerce.Application.Tests/Users/Queries/GetUserByIdQueryTests.cs
@@ -0,0 +1,20 @@
+﻿using ECommerce.Application.Users.Queries;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Application.Tests.Users.Queries;
+
+public class GetUserByIdQueryTests
+{
+    [Fact]
+    public void GetUserByIdQuery_ReturnsRecord()
+    {
+        // Arrange
+        var userId = Guid.NewGuid();
+        GetUserByIdQuery query = new(userId);
+        // Assert
+        query.UserId.Should().Be(userId);
+    }
+}
