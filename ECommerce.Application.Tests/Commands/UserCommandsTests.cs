55ca412a66373fbf33b8c2aa5a6a2181f7482caf
cloudjohnsp <jvavelino2017@gmail.com>
2026-09-06 23:00:21 -0300

    test: tests application layer user commands

---
+++ ECommerce.Application.Tests/Commands/UserCommandsTests.cs
@@ -0,0 +1,48 @@
+﻿using ECommerce.Application.Users.Commands;
+using ECommerce.Domain.Enums;
+using ECommerce.Shared.Results;
+using FluentAssertions;
+
+namespace ECommerce.Application.Tests.Commands;
+
+
+public class UserCommandsTests
+{
+    [Fact]
+    public void CreateUserCommand_ReturnsRecord()
+    {
+        // Arrange
+        CreateUserCommand user = new("John", "Doe", "john.doe@example.com", "123456@password", UserRole.Customer);
+        // Assert
+        user.FirstName.Should().Be("John");
+        user.LastName.Should().Be("Doe");
+        user.Email.Should().Be("john.doe@example.com");
+        user.Password.Should().Be("123456@password");
+        user.Role.Should().Be(UserRole.Customer);
+    }
+
+    [Theory]
+    [InlineData(UserRole.Customer)]
+    [InlineData(UserRole.Administrator)]
+    public void CreateUserCommand_WithValidRole_RetunsRecord(UserRole role)
+    {
+        // Arrange
+        CreateUserCommand user = new("John", "Doe", "john.doe@example.com", "123456@password", role);
+        // Assert
+        user.FirstName.Should().Be("John");
+        user.LastName.Should().Be("Doe");
+        user.Email.Should().Be("john.doe@example.com");
+        user.Password.Should().Be("123456@password");
+        user.Role.Should().Be(role);
+    }
+
+    [Fact]
+    public void DeleteUserCommand_ReturnsRecord()
+    {
+        // Arrange
+        var userId = Guid.NewGuid();
+        DeleteUserCommand command = new(userId);
+        // Assert
+        command.UserId.Should().Be(userId);
+    }
+}
