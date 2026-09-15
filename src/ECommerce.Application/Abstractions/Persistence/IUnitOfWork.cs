5bdd30d3861e3a2b231470b45871bd53f93383b9
cloudjohnsp <jvavelino2017@gmail.com>
2026-09-14 23:19:45 -0300

    feat: adds transaction method for unit of work class

--- src/ECommerce.Application/Abstractions/Persistence/IUnitOfWork.cs
+++ src/ECommerce.Application/Abstractions/Persistence/IUnitOfWork.cs
@@ -3,4 +3,9 @@ namespace ECommerce.Application.Abstractions.Persistence;
 public interface IUnitOfWork
 {
     Task<int> Commit(CancellationToken cancellationToken = default);
+
+    // Transaction management methods
+    Task BeginTransactionAsync(CancellationToken cancellationToken = default);
+    Task CommitTransactionAsync(CancellationToken cancellationToken = default);
+    Task RollbackTransactionAsync(CancellationToken cancellationToken = default);
 }
--- src/ECommerce.Persistence/UnitOfWork.cs
+++ src/ECommerce.Persistence/UnitOfWork.cs
@@ -1,15 +1,59 @@
 ﻿using ECommerce.Application.Abstractions.Persistence;
 using ECommerce.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore.Storage;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ECommerce.Persistence;
 
-public class UnitOfWork(AppDbContext _context) : IUnitOfWork
+public sealed class UnitOfWork(AppDbContext context) : IUnitOfWork
 {
+    private IDbContextTransaction? _currentTransaction;
+
     public async Task<int> Commit(CancellationToken cancellationToken = default)
     {
-        return await _context.SaveChangesAsync(cancellationToken);
+        return await context.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task BeginTransactionAsync(CancellationToken cancellationToken = default)
+    {
+        _currentTransaction = await context.Database.BeginTransactionAsync(cancellationToken);
+    }
+
+
+    public async Task CommitTransactionAsync(CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            await context.SaveChangesAsync(cancellationToken);
+            if (_currentTransaction != null)
+            {
+                await _currentTransaction.CommitAsync(cancellationToken);
+            }
+        }
+        catch
+        {
+            await RollbackTransactionAsync(cancellationToken);
+            throw;
+        }
+        finally
+        {
+            if (_currentTransaction != null)
+            {
+                await _currentTransaction.DisposeAsync();
+                _currentTransaction = null;
+            }
+        }
+    }
+
+    public async Task RollbackTransactionAsync(CancellationToken cancellationToken = default)
+    {
+        if (_currentTransaction != null)
+        {
+            await _currentTransaction.RollbackAsync();
+            await _currentTransaction.DisposeAsync();
+            _currentTransaction = null;
+        }
     }
 }
